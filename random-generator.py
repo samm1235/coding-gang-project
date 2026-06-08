7b599b61dd589fd1b93baf456e12e55b37efe8d6
samm1235 <samanthachongkaiwing67@gmail.com>
2026-06-08 10:26:24 +0000

    sign off for now

--- random-generator.py
+++ random-generator.py
@@ -5,10 +5,14 @@ def search():
         matching_indices = []
 
         prompt = input("Any filters? (e.g. cuisine, location):")
+        
         if prompt == 0:
             rand_num = random.randint()
             matching_indices.append(rand_num)
+
         elif isinstance(prompt,str):
-            for index, lines in enumerate(library.csv):
+            for index, lines in enumerate(csvextractor.clean_dict()):
                 if prompt in lines:
                     matching_indices.append(index)
+
+    return None
