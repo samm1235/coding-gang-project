49ee04405b7a962c402a188cd6b54cbcbf43d0df
samm1235 <samanthachongkaiwing67@gmail.com>
2026-06-19 14:47:25 +0000

    yay

--- test-for-code.py
+++ test-for-code.py
@@ -1,7 +1,5 @@
 # use this to run the whole code!
 import main
-import csv
 
-main.run_library("library.csv")
-# food_options = main.load_food_option("library.csv")
-# print(food_options)
+manager = main.Food_manager("library.csv")
+manager.run()
